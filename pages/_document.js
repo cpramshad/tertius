88bc7b47556c64618be6873e02cd80a0083cd030
Ramshad <cpramshad@gmail.com>
2023-01-05 11:43:09 +0400

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-4NWF3ZVNCF"
+        />
+
+        <Script id="gtm-test" strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-4NWF3ZVNCF', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
